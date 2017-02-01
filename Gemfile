59907d92cf633d303d298c97fb295ba763608f9f
awongh <akira@awongh.com>
2017-02-01 19:32:36 +0700

    stuff

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+ruby "2.3.1"
+
+gem 'swe4r'
+
+group :test do
+  gem 'rspec-its'
+  gem 'rspec-rails'
+end
---
+++ spec/unit/chart_data_spec.rb
@@ -0,0 +1,29 @@
+require 'swe4r'
+
+describe "Chart Data" do
+
+  it 'calculates degrees the same on all systems' do
+
+    time = 363038400
+
+    latitude = 37.7749295
+
+    longitude = -122.41941550000001
+
+    Swe4r::swe_set_topo(longitude, latitude, 0)
+
+    date = Time.at( time ).to_datetime
+
+    hour = date.hour + ( date.min / 60 )
+
+    jd = Swe4r::swe_julday(date.year, date.month, date.day, hour)
+
+    body = Swe4r::swe_calc_ut(jd, 1, Swe4r::SEFLG_MOSEPH|Swe4r::SEFLG_TOPOCTR)
+    longitude = body[0]
+
+    #run tests
+    expect(jd).to equal(2444789.625)
+    expect(longitude).to equal(132.34025732269768)
+
+  end
+end
